3bd81df372ff95d61dce2fe5ac5d41f7b79402cd
Sebastian Panceac <spanceac@gmail.com>
2025-02-14 01:33:53 +0200

    Disable UART RX during flash erasing
    After the MCU receives over UART the start sequence from the host flashing tool,
    it will start erase its flash. After the flash erase operation is done, it will
    send its ready message to the host flashing tool.
    
    The host flashing tool sends its start sequence in a loop until it receives the
    MCU ready message. If we have a bigger amount of flash we need to erase, the
    receival of the start sequence can cause an overrun in the UART RX, because no
    one handles RX bytes during flash erase operation. This RX overrun will stop MCU
    from receiving the flashing data from the host tool after MCU sends its ready
    message.
    
    This commit stops the UART RX during flash erase operation and enables it
    back after it sends to host flashing tool its ready message. This way any
    UART message that is sent by the flashing tool during the flash erase process
    will not reach the UART FIFO and we avoid RX overruns
    
    Signed-off-by: Sebastian Panceac <spanceac@gmail.com>

--- bootloader/uart/uart.h
+++ bootloader/uart/uart.h
@@ -1,8 +1,25 @@
 #include <stdbool.h>
 
-void uart_init(void);
+#include <xc.h>
+
+enum rx_state {
+    RX_STATE_DISABLED,
+    RX_STATE_ENABLED,
+};
+
+void uart_init(enum rx_state rx_state);
 int uart_get_byte(uint8_t *byte, size_t timeout_us, bool block);
 void uart_write_byte(uint8_t byte);
 int uart_expect_msg(char *msg, size_t len, size_t timeout_us);
 void uart_send_buf(uint8_t *buf, size_t cnt);
+
+static inline void uart_rx_disable(void)
+{
+    RCSTA1bits.CREN = 0;
+}
+
+static inline void uart_rx_enable(void)
+{
+    RCSTA1bits.CREN = 1;
+}
 
